e9e39772bbf159e4b7328b51ca70d97a3e1af946
idrapeau <73242450+idrapeau@users.noreply.github.com>
2024-08-27 19:22:33 +0000

    Make code more efficient and add error handling in download_files_idrapeau.py

--- src/15 Download Sequential Files/download_files_idrapeau.py
+++ src/15 Download Sequential Files/download_files_idrapeau.py
@@ -9,44 +9,46 @@ def download_files(sequence_url, output_directory):
     os.makedirs(output_directory)
 
   url_components = sequence_url.split("/")
-
   base_url = sequence_url.partition(url_components[-1])[0]
-
   file_name_1 = url_components[-1]
 
   # Split the file name into the name and extension
   file_name_components = file_name_1.split(".")
-
   # Remove the numbers from the name
   file_name_components[0] = ''.join(re.findall(r'[a-zA-Z]+', file_name_components[0]))
 
-  # Initialize a list of file names
-  file_name_list = [file_name_1]
-
   # All of the file names are printed here
   request_url = urllib.request.urlopen(base_url)
-  request_url_txt = request_url.read()
-  request_url_txt = str(request_url_txt)
+  request_url_txt = str(request_url.read())
 
-  request_url_txt = request_url_txt.replace(file_name_1, "")
+  file_url = base_url + "/" + file_name_1
+  file_path = output_directory + "/" + file_name_1
+
+  try:
+    urllib.request.urlretrieve(file_url, file_path)
+  except:
+    print("Download failed (likely due to an invalid file name or path)")
+  else:
+    # Remove the file name that was added to the list
+    request_url_txt = request_url_txt.replace(file_name_1, "")
 
   while file_name_components[0] in request_url_txt:
     # Find a file name of the correct format and add it to the list
     start_index = request_url_txt.find(file_name_components[0])
     end_index = request_url_txt.find(file_name_components[1]) + len(file_name_components[1])
-    next_file_name = request_url_txt[start_index:end_index]
-    file_name_list.append(next_file_name)
-
-    # I might want to verify that this has found a valid file name
-
-    # Remove the file name that was added to the list
-    request_url_txt = request_url_txt.replace(next_file_name, "")
-
-  # Download all files on the list to the specified output directory
-  for file_name in file_name_list:
+    file_name = request_url_txt[start_index:end_index]
+    
+    # Download the file to the specified output directory
     file_url = base_url + "/" + file_name
     file_path = output_directory + "/" + file_name
-    urllib.request.urlretrieve(file_url, file_path)
+
+    try:
+      urllib.request.urlretrieve(file_url, file_path)
+    except:
+      print("Download failed (likely due to an invalid file name or path)")
+    else:
+      # Remove the file name that was added to the list
+      request_url_txt = request_url_txt.replace(file_name, "")
 
 
 
