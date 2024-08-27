6e9e63f16a12d3650c3e343e668c10c1e55ef57c
idrapeau <73242450+idrapeau@users.noreply.github.com>
2024-08-27 18:49:29 +0000

    Remove print statements

--- src/15 Download Sequential Files/download_files_idrapeau.py
+++ src/15 Download Sequential Files/download_files_idrapeau.py
@@ -5,21 +5,15 @@ import re
 def download_files(sequence_url, output_directory):
   url_components = sequence_url.split("/")
 
-  #print(url_components)
-
   base_url = sequence_url.partition(url_components[-1])[0]
 
-  #print(base_url)
-
   file_name_1 = url_components[-1]
-  print(file_name_1)
 
   # Split the file name into the name and extension
   file_name_components = file_name_1.split(".")
-  print(file_name_components)
+
   # Remove the numbers from the name
   file_name_components[0] = ''.join(re.findall(r'[a-zA-Z]+', file_name_components[0]))
-  print(file_name_components)
 
   # Initialize a list of file names
   file_name_list = [file_name_1]
@@ -31,14 +25,13 @@ def download_files(sequence_url, output_directory):
   request_url = urllib.request.urlopen(base_url)
   request_url_txt = request_url.read()
   request_url_txt = str(request_url_txt)
-  #print(request_url_txt)
 
   request_url_txt = request_url_txt.replace(file_name_1, "")
 
   while file_name_components[0] in request_url_txt:
     # Find a file name of the correct format and add it to the list
     start_index = request_url_txt.find(file_name_components[0])
-    end_index = request_url_txt.find(file_name_components[1]) + 3
+    end_index = request_url_txt.find(file_name_components[1]) + len(file_name_components[1])
     next_file_name = request_url_txt[start_index:end_index]
     file_name_list.append(next_file_name)
 
